fa0d3f04f832d9011284a39b66fe38ceeb1c7178
OnlyM1ss <sojiguy@mail.ru>
2020-07-14 03:47:56 +0300

    fix Tests(All case is validable)

--- CalcTest/src/test/java/CalculatorTest.java
+++ CalcTest/src/test/java/CalculatorTest.java
@@ -4,8 +4,6 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import java.util.Locale;
-
 public class CalculatorTest extends Assert {
     Calculator calculator = new Calculator();
 
@@ -22,10 +20,10 @@ public class CalculatorTest extends Assert {
     @DataProvider(name = "positiveSumData")
     public Object[][] CalcSumData() {
         return new Object[][]{
-                {3, 6, 9},
-                {12, 3, 15},
-                {13, 22, 35},
-                {12342, 123, 12465},
+                {"3", "6", 9},
+                {"12", "3", 15},
+                {"13", "22", 35},
+                {"12342", "123", 12465},
         };
     }
 
@@ -33,77 +31,95 @@ public class CalculatorTest extends Assert {
     @DataProvider(name = "positiveMultiplyData")
     public Object[][] CalcMultyplyData() {
         return new Object[][]{
-                {3, 6, 18},
-                {12, 3, 36},
-                {13, 22, 286},
-                {12, 12, 144},
+                {"3", "6", 18},
+                {"12", "3", 36},
+                {"13", "22", 286},
+                {"12", "12", 144},
         };
     }
 
+    //дд для позитивных кейсов для разницы
     @DataProvider(name = "positiveDifferenceData")
     public Object[][] CalcDifferenceData() {
         return new Object[][]{
-                {3, 6, -3},
-                {12, 3, 9},
-                {13, 22, -9},
-                {12, 12, 0},
+                {"3", "6", -3},
+                {"12", "3", 9},
+                {"13", "22", -9},
+                {"12", "12", 0},
         };
     }
 
+    //дд для позитивных кейсов для деления
     @DataProvider(name = "positiveDivvyData")
     public Object[][] CalcDivvyData() {
         return new Object[][]{
-                {6, 2, 3},
-                {12, 3, 4},
-                {100, 2, 50},
-                {12, 12, 1},
+                {"6", "2", 3},
+                {"12", "3", 4},
+                {"100", "2", 50},
+                {"12", "12", 1},
+        };
+    }
+
+    //дд для негативных тестов
+    @DataProvider(name = "NegativeCalcData")
+    public Object[][] CalcDataNegative() {
+        return new Object[][]{
+                {"seven", 2, 9},
+                {true, 3, 123},
+                {null, 2, 50},
+                {1.2, 12, 1},
         };
     }
 
     @Test(dataProvider = "positiveMultiplyData")
-    public void testMultiply(int a, int b, int expected) {
+    public void testMultiply(String a, String b, int expected) throws Exception {
         //Позитивные кейсы
-        assertEquals(calculator.multyply(a, b), expected);
-        //Негативные кейсы
+        assertEquals(calculator.multiply(a, b), expected);
     }
 
     @Test(dataProvider = "positiveSumData")
-    public void testSum(int a, int b, int expected) {
+    public void testSum(String a, String b, int expected) throws Exception {
         assertEquals(calculator.sum(a, b), expected);
     }
 
     @Test(dataProvider = "positiveDifferenceData")
-    public void testDifference(int a, int b, int expected) {
+    public void testDifference(String a, String b, int expected) throws Exception {
         assertEquals(calculator.difference(a, b), expected);
 
     }
 
     @Test(dataProvider = "positiveDivvyData")
-    public void testDivvy(int a, int b, int expected) {
+    public void testDivvy(String a, String b, int expected) throws Exception {
         Assert.assertEquals(calculator.divvy(a, b), expected);
     }
 
+    //@Test(dataProvider = "NegativeCalcData", expectedExceptions = {ClassCastException.class, NullPointerException.class})
+    //public void testMultiplyNegative(String a, String b, Object expected) {
+      //  Assert.assertEquals(calculator.multyply(a, b),expected);
+    //}
+
     @Test
-    public void testMultiplyCommon() {
-        Assert.assertEquals(calculator.multyply(3, 2), 6);
-        Assert.assertEquals(calculator.multyply(300, 2), 600);
+    public void testMultiplyCommon() throws Exception {
+        Assert.assertEquals(calculator.multiply("3", "2"), 6);
+        Assert.assertEquals(calculator.multiply("300", "2"), 600);
     }
 
     @Test
-    public void testSumCommon() {
-        Assert.assertEquals(calculator.sum(3, 2), 5);
-        Assert.assertEquals(calculator.sum(134, 22), 156);
+    public void testSumCommon() throws Exception {
+        Assert.assertEquals(calculator.sum("3", "2"), 5);
+        Assert.assertEquals(calculator.sum("134", "22"), 156);
     }
 
     @Test
-    public void testDifferenceCommon() {
-        Assert.assertEquals(calculator.difference(134, 22), 112);
-        Assert.assertEquals(calculator.difference(133, 3), 130);
+    public void testDifferenceCommon() throws Exception {
+        Assert.assertEquals(calculator.difference("134", "22"), 112);
+        Assert.assertEquals(calculator.difference("133", "3"), 130);
     }
 
     @Test
-    public void testDivvyCommon() {
-        Assert.assertEquals(calculator.divvy(100, 5), 20);
-        Assert.assertEquals(calculator.divvy(200, 50), 4);
+    public void testDivvyCommon() throws Exception {
+        Assert.assertEquals(calculator.divvy("100", "5"), 20);
+        Assert.assertEquals(calculator.divvy("200", "50"), 4);
     }
+
 }
