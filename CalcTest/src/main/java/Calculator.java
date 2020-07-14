58839727294eeb28c165f504ac1cfca44f979f43
OnlyM1ss <sojiguy@mail.ru>
2020-07-14 03:47:07 +0300

    fix Calc(it's can take all value and cast in int)

--- CalcTest/src/main/java/Calculator.java
+++ CalcTest/src/main/java/Calculator.java
@@ -7,8 +7,11 @@ public class Calculator {
      * @param b - Второй множитель
      * @return a*b
      */
-    public int multyply(int a, int b) {
-        return a * b;
+    public int multiply(String a, String b) throws Exception {
+        if(HelpFunction.tryParseInt(a,b))   {
+            return Integer.parseInt(a)*Integer.parseInt(b);
+        }
+        throw new Exception("Невозможно преобразовать в число");
     }
 
     /**
@@ -16,27 +19,34 @@ public class Calculator {
      * @param b - Второе число
      * @return
      */
-    public int sum(int a, int b) {
-        return a + b;
+    public int sum(String a, String b) throws Exception {
+        if(HelpFunction.tryParseInt(a,b))   {
+            return Integer.parseInt(a)+Integer.parseInt(b);
+        }
+        throw new Exception("Невозможно преобразовать в число");
     }
 
     /**
-     *
      * @param a - Из чего вычитают
      * @param b - Что вычитают
      * @return разницу между a и b
      */
-    public int difference(int a, int b) {
-        return a - b;
+    public int difference(String a, String b) throws Exception {
+        if(HelpFunction.tryParseInt(a,b))   {
+            return Integer.parseInt(a)-Integer.parseInt(b);
+        }
+        throw new Exception("Невозможно преобразовать в число");
     }
 
     /**
-     *
      * @param a - Что делят
      * @param b - На что делят
      * @return a/b
      */
-    public int divvy(int a, int b) {
-        return a / b;
+    public int divvy(String a, String b) throws Exception {
+        if(HelpFunction.tryParseInt(a,b))   {
+            return Integer.parseInt(a)/Integer.parseInt(b);
+        }
+        throw new Exception("Невозможно преобразовать в число");
     }
 }
